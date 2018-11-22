5486fcc360dc172d823c8bbdcf616e6ab6cfed5a
mavregg <wild_power@list.ru>
2018-11-22 19:38:49 +0300

    fix url

--- src/actions/listActions.js
+++ src/actions/listActions.js
@@ -1,5 +1,5 @@
 const DEFAULT_QUERY = 'redux';
-const PATH_BASE = 'https://hn.alia.com/api/v1';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 export const fetchData = () => {
